944d68ae61efc002f48b3d9cf33e9b4223ecf387
russ <117883052+Russel-0@users.noreply.github.com>
2023-10-27 21:23:10 +0800

    save

--- math_1.py
+++ math_1.py
@@ -3,3 +3,12 @@ import math
 list_number = [1,2,2,3,4,3]
 
 print(max(list_number))
+
+number = 4.50
+
+print(round(number))
+print(math.ceil(number))
+print(math.floor(number))
+print(abs(number))
+print(math.sqrt(number))
+print(pow(number, 2))
