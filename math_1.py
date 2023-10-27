8b992303cfe26cc24f1926cebdca2a6ab04cdd29
russ <117883052+Russel-0@users.noreply.github.com>
2023-10-27 21:47:51 +0800

    save

--- math_1.py
+++ math_1.py
@@ -1,14 +1,24 @@
 import math
 
-list_number = [1,2,2,3,4,3]
+# list_number = [1,2,2,3,4,3]
 
-print(max(list_number))
+# print(max(list_number))
 
-number = 4.50
+# number = 4.50
 
-print(round(number))
-print(math.ceil(number))
-print(math.floor(number))
-print(abs(number))
-print(math.sqrt(number))
-print(pow(number, 2))
+# print(round(number))
+# print(math.ceil(number))
+# print(math.floor(number))
+# print(abs(number))
+# print(math.sqrt(number))
+# print(pow(number, 2))
+
+#   index and slicing
+
+name = "Russel Calderon"
+
+first = name[0:6]
+last = name[7:]
+reversed = name[::-1]
+
+print(reversed)
