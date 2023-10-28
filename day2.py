3562ac48423cab8eb4e133cd8f8ae9663440ef79
russ <117883052+Russel-0@users.noreply.github.com>
2023-10-28 22:12:20 +0800

    save

--- day2.py
+++ day2.py
@@ -1,7 +1,24 @@
+import time
 
-name = None
+# name = ""
 
-while not name:
-    name = input("Enter your name: ")
+# while len(name) == 0:
+#     name = input("Enter your name: ")
 
-print(name)
+#     if len(name) == 0:
+#         print("please enter name")
+#         name = ""
+#     elif len(name) < 3:
+#         print("your name is too short")
+#         name = ""
+#     else:
+#         print("Hello " + name)
+
+# for i in range(10):
+#     print(i+1)
+    
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(0.5)
+
+print("BOOM!")
