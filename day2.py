a40b910a43b6a895cef68649f803417bb1fbb9ff
russ <117883052+Russel-0@users.noreply.github.com>
2023-10-28 22:44:05 +0800

    save

--- day2.py
+++ day2.py
@@ -17,6 +17,18 @@ import time
 # for i in range(10):
 #     print(i+1)
     
-for seconds in range(10,0,-1):
-    print(seconds)
-    time.sleep(0.5)
+# for seconds in range(10,0,-1):
+#     print(seconds)
+#     time.sleep(0.5)
+
+# while True:
+#     name = input("Enter your name:")
+#     if name != "":
+#         break
+
+data = "R.u.s.s.e.l"
+
+for i in data:
+    if i == ".":
+        continue
+    print(i, end="")
